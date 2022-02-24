c70f25d9c241322c9165985c1d75fa8a54b3a621
artem_priadkin <artem_priadkin@epam.com>
2022-02-24 18:21:41 +0100

    add simple nav menu

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
-import {Layout} from 'antd';
+import {
+    Layout,
+    Typography,
+} from 'antd';
+import cx from 'classnames';
+import {Svg} from 'src/uiKit';
 
-import {Svg} from '../../../uiKit';
+import styles from './Header.module.scss';
+
+const {Text} = Typography;
 
 const {Header: HeaderLayout} = Layout;
 
 export const Header = () => (
-    <HeaderLayout>
+    <HeaderLayout className={styles.headerLayout}>
         <div className="container position-fixed">
-            <Svg iconName="ll-logo-green" className="logo" />
+            <div className={styles.headerWrapper}>
+                <Svg iconName="ll-logo-green" className={styles.logo} />
+                <ul className={cx(styles.navMenu, 'd-flex align-items-center')}>
+                    <li><Text className="header-menu-item">Search</Text></li>
+                    <li><Text className="header-menu-item">Log in</Text></li>
+                    <li><Text className="header-menu-item">Register</Text></li>
+                    <li><Text className="header-menu-item">To checkout</Text></li>
+                </ul>
+            </div>
         </div>
     </HeaderLayout>
 );
