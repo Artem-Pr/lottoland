f2aae17fb6b7e3c1c4eedcebd7b65cf1adf047c5
artem_priadkin <artem_priadkin@epam.com>
2022-02-24 21:39:34 +0100

    add header search field

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
     Badge,
+    Form,
+    Input,
     Layout,
     Popover,
     Typography,
@@ -16,39 +18,72 @@ const {Text} = Typography;
 
 const {Header: HeaderLayout} = Layout;
 
-export const Header = () => (
-    <HeaderLayout className={styles.headerLayout}>
-        <div className="container position-fixed">
-            <div className={styles.headerWrapper}>
-                <Svg iconName="ll-logo-green" className={styles.logo} />
-                <ul className={cx(styles.navMenu, 'd-flex align-items-center')}>
-                    <li><Text className={cx(styles.fontIcon, styles.search, 'header-menu-item')} /></li>
-                    <li><Text className="header-menu-item">Log in</Text></li>
-                    <li><Text className="header-menu-item">Register</Text></li>
-                    <li>
-                        <Popover
-                            placement="bottomRight"
-                            content={EmptyCartMessage}
-                            arrowPointAtCenter
+export const Header = () => {
+    const [showSearchField, setShowSearchField] = useState(false);
+
+    const handleSearchButtonClick = () => {
+        setShowSearchField(!showSearchField);
+    };
+
+    return (
+        <HeaderLayout className={styles.headerLayout}>
+            <div className="container position-fixed">
+                <div className={styles.headerWrapper}>
+                    <Svg iconName="ll-logo-green" className={styles.logo} />
+                    {showSearchField && (
+                        <Form
+                            className={styles.form}
+                            layout="vertical"
                         >
-                            <Text
-                                className={cx(
-                                    styles.headerMenuItem,
-                                    'd-flex align-items-center header-menu-item',
-                                )}
+                            <Form.Item
+                                label="Search for Lottoland products"
+                                name="SearchField"
                             >
-                                <Badge count={0} showZero>
-                                    <div className={cx(styles.fontIcon, styles.cart)} />
-                                </Badge>
-                                <div className={cx(styles.walletWrapper, 'd-flex flex-column')}>
-                                    <Text className="wallet">&euro;0.00</Text>
-                                    <span className={styles.toCheckout}>To checkout</span>
-                                </div>
-                            </Text>
-                        </Popover>
-                    </li>
-                </ul>
+                                <Input
+                                    className={styles.formInput}
+                                    bordered={false}
+                                />
+                            </Form.Item>
+                        </Form>
+                    )}
+                    <ul className={cx(styles.navMenu, 'd-flex align-items-center')}>
+                        <li>
+                            <Text
+                                onClick={handleSearchButtonClick}
+                                className={cx(
+                                    styles.fontIcon,
+                                    showSearchField ? styles.close : styles.search,
+                                    'header-menu-item',
+                                )}
+                            />
+                        </li>
+                        <li><Text className="header-menu-item">Log in</Text></li>
+                        <li><Text className="header-menu-item">Register</Text></li>
+                        <li>
+                            <Popover
+                                placement="bottomRight"
+                                content={EmptyCartMessage}
+                                arrowPointAtCenter
+                            >
+                                <Text
+                                    className={cx(
+                                        styles.headerMenuItem,
+                                        'd-flex align-items-center header-menu-item',
+                                    )}
+                                >
+                                    <Badge count={0} showZero>
+                                        <div className={cx(styles.fontIcon, styles.cart)} />
+                                    </Badge>
+                                    <div className={cx(styles.walletWrapper, 'd-flex flex-column')}>
+                                        <Text className="wallet">&euro;0.00</Text>
+                                        <span className={styles.toCheckout}>To checkout</span>
+                                    </div>
+                                </Text>
+                            </Popover>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
-    </HeaderLayout>
-);
+        </HeaderLayout>
+    );
+};
